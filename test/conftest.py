a49321300edfed8f1911d1996fb503ae16bd6e77
vladicom <michejew@gmail.com>
2024-01-09 10:55:15 +0100

    Update conftest

--- test/conftest.py
+++ test/conftest.py
@@ -1,7 +1,4 @@
 import pytest
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
@@ -11,9 +8,10 @@ import allure
 from testdata.DataProvider import DataProvider
 from pages.authPage import AuthPage
 from pages.apiPage import ApiPage
+from pages.subForm import MyPage
 from configurations.ConfigProvider import ConfigProvider
 
-@pytest.fixture()
+@pytest.fixture(scope="session")
 def browser():
     with allure.step('Open and configure browser'):
         timeout = ConfigProvider().getint("ui", "timeout")
@@ -28,17 +26,157 @@ def browser():
     with allure.step('Close browser'):
         browser.quit()
 
-@pytest.fixture
-def token(browser):
-        auth = AuthPage(browser)
-        auth.get_login()
-        auth.enter_email("user", DataProvider().e_mail())
-        auth.submit_form("login")
-        auth.enter_password("password", DataProvider().password)
-        auth.submit_form("login-submit")
-        cookie = auth.set_cookie()
-        #return cookie
+@pytest.fixture(scope="module")
+def log_in(browser) -> AuthPage:
+    log_page = AuthPage(browser)
+    with allure.step("Open page"):
+        log_page.get_login(
+            base_url=ConfigProvider().ui_url(), log_path=DataProvider().get("log_path")
+        )
+    with allure.step("Autorization on the page"):
+        log_page.enter_email("user", DataProvider().e_mail())
+        log_page.submit_form("login")
+        log_page.enter_password("password", DataProvider().password())
+        log_page.submit_form("login-submit")
+        log_page.set_cookie()
+    yield browser
 
+@pytest.fixture
+def ui_page(browser) -> MyPage:
+    return MyPage(browser)
+
+@pytest.fixture
+def ui_board_list(browser) -> dict:
+    lst = MyPage(browser)
+    with allure.step("Open boards list"):
+        return lst.get_item_list(DataProvider().get("ui_board_list"))
+
+@pytest.fixture
+def get_add_ui_board(ui_page: MyPage) -> dict:
+    new_board = ui_page
+    with allure.step("Click on 'Create new board'"):
+        new_board.open_editor(DataProvider().get("ui_new_board"))
+    with allure.step("Enter board name"):
+        new_board.create_item(
+            DataProvider().get("ui_enter_board_name"),
+            DataProvider().get("board_name")
+        )
+    with allure.step("Click button 'Create'"):
+        new_board.submit(DataProvider().get("ui_submit_board"))
+    return new_board.load_item(DataProvider().get("ui_board_content"))
+
+@pytest.fixture
+def update_list(ui_page: MyPage) -> dict:
+    ui_page.get_page(ConfigProvider().ui_url())
+    lst = ui_page.get_item_list(DataProvider().get("ui_board_list"))
+    with allure.step("Update item list"):
+        return lst[1:]
+
+@pytest.fixture
+def open_board(ui_page: MyPage, get_boards: list) -> MyPage:
+    boards = ui_page
+    board = get_boards[0]
+    board_title = board.get("name")
+    with allure.step(f"Open board {board_title}(click)"):
+        boards.get_item(DataProvider().get("ui_open_board"), board_title)
+
+@pytest.fixture
+def ui_card_list(browser) -> dict:
+    lst = MyPage(browser)
+    with allure.step("Open cards list"):
+        return lst.get_item_list(DataProvider().get("ui_card_list"))
+    
+@pytest.fixture
+def ui_card_new_list(browser) -> dict:
+    lst = MyPage(browser)
+    with allure.step("Open cards list"):
+        return lst.get_item_list(DataProvider().get("ui_card_list_id"))
+
+@pytest.fixture
+def get_add_ui_card(ui_page: MyPage) -> dict:
+    new_card = ui_page
+    with allure.step("On the 'ToDo' list click 'Add a card'"):
+        new_card.open_editor(DataProvider().get("ui_add_card"))
+    with allure.step("Enter Card name"):
+        new_card.create_item(
+            DataProvider().get("ui_new_card"),
+            DataProvider().get("card_name")
+        )
+    with allure.step("Click button 'Add card'"):
+        new_card.submit(DataProvider().get("ui_submit_card"))
+
+@pytest.fixture
+def edit_ui_card(ui_page: MyPage) -> dict:
+    new_card = ui_page
+    with allure.step("Open the card with click a card"):
+        new_card.open_editor(DataProvider().get("ui_card_menu"))
+    with allure.step("Enter new Card name"):
+        new_card.edit_field(DataProvider().get("ui_edit_card"))
+        new_card.create_item(
+            DataProvider().get("ui_edit_card"),
+            DataProvider().get("card_new")
+        )
+    with allure.step("Click button 'X' close"):
+        new_card.submit(DataProvider().get("ui_close_edit"))
+
+@pytest.fixture
+def delete_ui_card(ui_page: MyPage) -> dict:
+    new_card = ui_page
+    with allure.step("Open the card with click a card"):
+        new_card.open_editor(DataProvider().get("ui_card_menu"))
+    with allure.step("Click button 'Archive' in actions-menu"):
+        new_card.open_editor(DataProvider().get("ui_archive_card"))
+    with allure.step("Click button 'Delete' in actions-menu"):
+        new_card.open_editor(DataProvider().get("ui_delete_card"))       
+    with allure.step("Click button 'Delete'"):
+        new_card.submit(DataProvider().get("ui_close_edit"))
+
+@pytest.fixture
+def move_ui_card(ui_page: MyPage) -> dict:
+    new_card = ui_page
+    with allure.step("On the card with click a card"):
+        new_card.open_editor(DataProvider().get("ui_card_menu"))
+    with allure.step("Click button 'Move' in actions-menu"):
+        c_lst = new_card.get_item_list(DataProvider().get("ui_current_list"))[0]
+        #current =[c for c in lst]
+        new_card.open_editor(DataProvider().get("ui_move_card"))
+    with allure.step("Select another list('Doing')"):
+        new_card.open_editor(DataProvider().get("ui_select_list"))
+    with allure.step("Submit move card"):
+        new_card.submit(DataProvider().get("ui_submit_move"))
+        new_lst = new_card.get_item_list(DataProvider().get("ui_current_list"))[0]
+    return c_lst, new_lst
+
+@pytest.fixture
+def move_mouse_card(ui_page: MyPage) -> dict:
+    card = ui_page
+    card.open_editor(DataProvider().get("ui_card_menu"))
+    c_lst = card.get_item_list(DataProvider().get("ui_current_list"))[0]
+    card.submit(DataProvider().get("ui_close_edit"))
+    card.drag_and_drop_item(
+        DataProvider().get("dragg_card"),
+        DataProvider().get("drop_lists")
+    )
+    card.open_editor(DataProvider().get("ui_card_menu"))
+    n_lst = card.get_item_list(DataProvider().get("ui_current_list"))[0]
+    card.submit(DataProvider().get("ui_close_edit"))
+    return c_lst, n_lst
+
+@pytest.fixture
+def delete_board_ui(ui_page: MyPage, open_board):
+    with allure.step("Open board"):
+        menu = ui_page
+    with allure.step("Open Menu"):
+        menu.open_editor(DataProvider().get("ui_board_menu"))
+    with allure.step("Click on 'Close'"):
+        menu.open_editor(DataProvider().get("ui_close_board"))
+        with allure.step("Submit 'Close'"):
+            menu.submit(DataProvider().get("ui_submit_close"))
+    with allure.step("Click on 'permanently delete'"):
+        menu.open_editor(DataProvider().get("ui_delete_board"))
+        with allure.step("Submit 'Delete'"):
+            menu.submit(DataProvider().get("ui_submit_del_board"))
+        
 @pytest.fixture
 def api_client() -> ApiPage:
     return ApiPage(ConfigProvider().api_url(), DataProvider().get_token())
@@ -69,7 +207,6 @@ def work_path(board_path) -> str:
 def get_boards(work_path) -> list:
     api = ApiPage(ConfigProvider().api_url(), DataProvider().get_token())
     resp = api.get_item_list(work_path)
-    yield resp
     return resp
 
 @pytest.fixture
@@ -132,8 +269,7 @@ def dummy_card(card_path, get_lists: dict):
     }
     api = ApiPage(ConfigProvider().api_url(), DataProvider().get_token())
     resp = api.add_item(card_path, body)
-    return resp.get("id")
-    
+    return resp.get("id")   
 
 @pytest.fixture
 def card_edit() -> None:
